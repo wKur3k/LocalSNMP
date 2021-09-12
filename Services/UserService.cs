0d0f604baf9bb84b94f23d1dca0a5625edd05adc
wkur3k <kurek.wojciech.01@gmail.com>
2021-09-12 16:45:20 +0200

    Added TokenGeneration

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,8 +1,12 @@
 ﻿using LocalSNMP.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LocalSNMP.Services
@@ -19,6 +23,37 @@ namespace LocalSNMP.Services
             _passwordHasher = passwordHasher;
             _authenticationSettings = authenticationSettings;
         }
-        
+        public string GenerateJwtToken(LoginUserDto dto)
+        {
+            var user = _dbContext.Users.FirstOrDefault(u => u.Login == dto.Login);
+
+            if (user is null)
+            {
+                return "Login or Password is incorrect";
+            }
+            var result = _passwordHasher.VerifyHashedPassword(user, user.HashedPassword, dto.Password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return "Login or Password is incorrect";
+            }
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, $"{user.Login}"),
+                new Claim(ClaimTypes.Role, $"{user.Role}"),
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var datenow = DateTime.Now;
+            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+            var token = new JwtSecurityToken(_authenticationSettings.JwtIssuer,
+                _authenticationSettings.JwtIssuer,
+                claims,
+                expires: expires,
+                signingCredentials: cred);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
